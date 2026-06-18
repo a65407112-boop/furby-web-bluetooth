78fff421e6d2c25f54d9fa96fe19d0f488517c7e
a65407112-boop <a65407112@gmail.com>
2026-06-18 20:46:32 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -15,17 +15,26 @@
         ]
     },
     {
-        "file": "noimg2.dlc", 
-        "title": "Audio only",
+        "file": "Hyperbaiter_HACKED_fullsong.dlc",
+        "title": "Hyperbaiter FULL",
         "buttons": [
-            { "title": "Audio test ", "action": [75,0,4,4] }
+            { "title": "Hyper action 3", "action": [75,0,3,4] },
+            { "title": "Hyper action 4", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "Hyperbaiter_HACKED_truncated_AMFsize.dlc",
+        "title": "Hyperbaiter SAFE",
+        "buttons": [
+            { "title": "Safe action 3", "action": [75,0,3,4] },
+            { "title": "Safe action 4", "action": [75,0,4,4] }
         ]
     },
     {
         "file": "chilli.dlc", 
         "title": "Chilli",
         "buttons": [
-            { "title": "Chilli ", "action": [75,0,4,4] }
+            { "title": "Chilli", "action": [75,0,4,4] }
         ]
     }
 ]
